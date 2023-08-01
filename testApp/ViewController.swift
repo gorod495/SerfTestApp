3a3e024c421a6f6af0a1a27e5855f074dcb01690
gorod495 <gorod312bs@gmail.com>
2023-08-01 14:31:29 +0300

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Городецкий Илья on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
